8221c6dfeea7d078d53629b3cfacc2b35fb886b8
abhishekY2401 <abhiyd2401@gmail.com>
2022-07-13 21:01:47 +0530

    added routes for creating and getting all category

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,11 +1,26 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
-const Categories = require("../models/Category");
+const Category = require("../models/Category");
 
 // create a category
-router.post();
+router.post("/", async (req, res) => {
+  const newCat = new Category(req.body);
+  try {
+    const savedCat = await newCat.save();
+    res.status(200).json(savedCat);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // search a category
-router.get();
+router.get("/", async (req, res) => {
+  try {
+    const category = await Category.find();
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-exports.module = router;
+module.exports = router;
