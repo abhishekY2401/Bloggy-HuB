4b8f2db55be648e9eafd2f38df7583604b20073e
abhishekY2401 <abhiyd2401@gmail.com>
2022-07-13 21:00:26 +0530

    added routes for posts, category and image upload

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -16,9 +17,24 @@ mongoose
     console.log(err);
   });
 
-app.use("/api/auth", () => require("./routes/auth"));
-app.use("/api/users", () => require("./routes/users"));
-app.use("/api/users/posts", () => require("./routes/posts"));
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded!");
+});
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/posts", require("./routes/posts"));
+app.use("/api/category", require("./routes/categories"));
 
 app.listen(5000, () => {
   console.log("App is running");
