868afec7bd2626f3525f155112da7ab807a9b7dc
abhishekY2401 <abhiyd2401@gmail.com>
2022-08-07 23:26:52 +0530

    done routing for required links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,22 @@
 import "./App.css";
 import Topbar from "./topbar/Topbar.js";
-import creative_blogs from "./images/creative_blogs.webp";
-import BlogList from "./blogs/BlogPosting";
-import productivity from "./images/productivity.jpg";
-import desktopsetup from "./images/desktop-setup.jpg";
-import tech from "./images/tech-stuff.jpg";
+import Login from "./auth/Login";
+import Signup from "./auth/Signup";
+import About from "./blogs/About";
+import Home from "./topbar/Home";
 import Footer from "./footer/Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="mt-0 px-0">
       <Topbar />
-      <div className="flex">
-        <div className="bg-slate-800 flex flex-col text-center justify-center">
-          <h1 className="text-5xl font-bold text-center p-10 text-gray-100 font-sans">
-            Convert your creative thoughts in form of blogs
-          </h1>
-          <p className="text-xl text-gray-400 p-20">
-            Create blogs and help others with your content and earn side-by-side
-          </p>
-          <a
-            href="/"
-            className="text-xl text-slate-800 p-2 bg-gray-100 w-40 rounded-full inline-flex justify-center ml-60 hover:text-slate-100 hover:bg-slate-900"
-          >
-            Get Started
-          </a>
-        </div>
-        <div className="w-fit">
-          <img src={creative_blogs} alt="blog" className="object-contain" />
-        </div>
-      </div>
-
-      <div>
-        <h1 className="text-2xl text-center mt-16 font-bold">Recent Blogs</h1>
-      </div>
-      <div className="flex justify-around mt-20">
-        <BlogList
-          url={productivity}
-          text="productivity"
-          title="5 Tips for Productivity"
-          description="lorem ipsum ok bye fir milte hai chalo"
-        />
-
-        <BlogList
-          url={desktopsetup}
-          text="productivity"
-          title="5 Tips for Productivity"
-          description="lorem ipsum ok bye fir milte hai chalo"
-        />
-
-        <BlogList
-          url={tech}
-          text="productivity"
-          title="5 Tips for Productivity"
-          description="lorem ipsum ok bye fir milte hai chalo"
-        />
-      </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
     </div>
   );
